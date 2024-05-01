6685f93561f5f3d59a61550c78f08903535f654b
Ricky Mok <cskpmok@caida.org>
2024-05-01 00:14:44 -0700

    patch

--- aggressivescanners.go
+++ aggressivescanners.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"sort"
 	"sync"
+	"sync/atomic"
 
 	"github.com/google/gopacket"
 	"gonum.org/v1/gonum/stat"
@@ -16,16 +17,16 @@ type ScannerProfile struct {
 	Dest     map[netip.Addr]uint8
 	Port     map[string]uint8
 	PckCount uint32
-	DestLock sync.Mutex
+	DestLock *sync.Mutex
 }
 
 type ScannerMap struct {
 	Scanner     map[netip.Addr]*ScannerProfile
-	ScannerLock sync.RWMutex
+	ScannerLock *sync.RWMutex
 }
 
 func CreateScannerProfile() ScannerMap {
-	return ScannerMap{Scanner: make(map[netip.Addr]*ScannerProfile), ScannerLock: sync.RWMutex{}}
+	return ScannerMap{Scanner: make(map[netip.Addr]*ScannerProfile), ScannerLock: &sync.RWMutex{}}
 
 }
 
@@ -52,7 +53,7 @@ func (m ScannerMap) AddScannerProfile(p gopacket.Packet) {
 			dstepoint := p.TransportLayer().TransportFlow().EndpointType().String() + ":" + p.TransportLayer().TransportFlow().Dst().String()
 			m.ScannerLock.Lock()
 			if _, exist := m.Scanner[srcip]; !exist {
-				m.Scanner[srcip] = &ScannerProfile{Dest: make(map[netip.Addr]uint8), Port: make(map[string]uint8), DestLock: sync.Mutex{}}
+				m.Scanner[srcip] = &ScannerProfile{Dest: make(map[netip.Addr]uint8), Port: make(map[string]uint8), DestLock: &sync.Mutex{}}
 				m.ScannerLock.Unlock()
 				m.Scanner[srcip].DestLock.Lock()
 				m.Scanner[srcip].Dest[dstip] = 1
@@ -62,49 +63,49 @@ func (m ScannerMap) AddScannerProfile(p gopacket.Packet) {
 				m.ScannerLock.Unlock()
 				m.Scanner[srcip].DestLock.Lock()
 				if _, sexist := m.Scanner[srcip].Dest[dstip]; !sexist {
-					m[srcip].Dest[dstip] = 1
+					m.Scanner[srcip].Dest[dstip] = 1
 				}
-				if _, pexist := m[srcip].Port[dstepoint]; !pexist {
-					m[srcip].Port[dstepoint] = 1
-				} 
-				m[srcip].DestLock.Unlock()
+				if _, pexist := m.Scanner[srcip].Port[dstepoint]; !pexist {
+					m.Scanner[srcip].Port[dstepoint] = 1
+				}
+				m.Scanner[srcip].DestLock.Unlock()
 			}
 		} else {
 			m.ScannerLock.Lock()
 			if _, exist := m.Scanner[srcip]; !exist {
-				m.Scanner[srcip] = &ScannerProfile{Dest: make(map[netip.Addr]int), Port: make(map[string]int), DestLock: sync.Mutex{}}
+				m.Scanner[srcip] = &ScannerProfile{Dest: make(map[netip.Addr]uint8), Port: make(map[string]uint8), DestLock: &sync.Mutex{}}
 				m.ScannerLock.Unlock()
 				m.Scanner[srcip].DestLock.Lock()
-				m[srcip].Dest[dstip] = 1
+				m.Scanner[srcip].Dest[dstip] = 1
 			} else {
 				m.ScannerLock.Unlock()
 				m.Scanner[srcip].DestLock.Lock()
-				if _, sexist := m[srcip].Dest[dstip]; !sexist {
-					m[srcip].Dest[dstip] = 1
-				} 
+				if _, sexist := m.Scanner[srcip].Dest[dstip]; !sexist {
+					m.Scanner[srcip].Dest[dstip] = 1
+				}
 			}
 			m.Scanner[srcip].DestLock.UnLock()
 		}
 	}
-	m[srcip].PckCount++
+	atomic.AddUint32(&m.Scanner[srcip].PckCount, 1)
 }
 
 func (m ScannerMap) MergeScannerMap(mnew ScannerMap) {
 	//merge mnew into m
-	for k, v := range mnew {
-		if _, exist := m[k]; !exist {
-			m[k] = v
+	for k, v := range mnew.Scanner {
+		if _, exist := m.Scanner[k]; !exist {
+			m.Scanner[k] = v
 		} else {
-			m[k].PckCount += v.PckCount
+			m.Scanner[k].PckCount += v.PckCount
 			for dk, dv := range v.Dest {
-				if _, dexist := m[k].Dest[dk]; !dexist {
-					m[k].Dest[dk] = dv
-				} 
+				if _, dexist := m.Scanner[k].Dest[dk]; !dexist {
+					m.Scanner[k].Dest[dk] = dv
+				}
 			}
 			for pk, pv := range v.Port {
-				if _, pexist := m[k].Port[pk]; !pexist {
-					m[k].Port[pk] = pv
-				} 
+				if _, pexist := m.Scanner[k].Port[pk]; !pexist {
+					m.Scanner[k].Port[pk] = pv
+				}
 			}
 		}
 	}
@@ -125,14 +126,14 @@ func (m ScannerMap) OutputJSON(fname string) {
 	}
 }
 
-func (m ScannerMap) OutputStat(fname string){
+func (m ScannerMap) OutputStat(fname string) {
 	outfile, err := os.Create(fname)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outfile.Close()
 	for k, v := range m.Scanner {
-		outfile.WriteString(k.String() + "," + string(len(v.Dest)) +"," +string(len(v.Port))+"\n")
+		outfile.WriteString(k.String() + "," + string(len(v.Dest)) + "," + string(len(v.Port)) + "\n")
 	}
 }
 
@@ -140,7 +141,7 @@ func (m ScannerMap) GetAggressiveScannersAD() []netip.Addr {
 	//Address dispersion
 	var ad []netip.Addr
 	theshold := 11000000 * 0.1 / 24 //one hour
-	for k, v := range m {
+	for k, v := range m.Scanner {
 		if len(v.Dest) > int(theshold) {
 			ad = append(ad, k)
 		}
@@ -153,12 +154,12 @@ func (m ScannerMap) GetAggressiveScannersPV() []netip.Addr {
 	var pv []netip.Addr
 	var vol []float64
 	theshold := 1 - 0.0001
-	for _, v := range m {
+	for _, v := range m.Scanner {
 		vol = append(vol, float64(v.PckCount))
 	}
 	sort.Float64s(vol)
 	th := stat.CDF(theshold, stat.Empirical, vol, nil)
-	for k, v := range m {
+	for k, v := range m.Scanner {
 		if float64(v.PckCount) >= th {
 			pv = append(pv, k)
 		}
@@ -171,12 +172,12 @@ func (m ScannerMap) GetAggressiveScannersDP() []netip.Addr {
 	var dp []netip.Addr
 	var dport []float64
 	theshold := 1 - 0.0001
-	for _, v := range m {
+	for _, v := range m.Scanner {
 		dport = append(dport, float64(len(v.Port)))
 	}
 	sort.Float64s(dport)
 	th := stat.CDF(theshold, stat.Empirical, dport, nil)
-	for k, v := range m {
+	for k, v := range m.Scanner {
 		if float64(len(v.Port)) >= th {
 			dp = append(dp, k)
 		}
